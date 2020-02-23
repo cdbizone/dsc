c849e4158ff0abfe07a5f9221478d6c86d49d0d3
ievgen <cdbiozne@gmail.com>
2020-02-23 18:33:37 +0200

    add filter, add plist for iOS

--- src/app/cars/car-list.component.ts
+++ src/app/cars/car-list.component.ts
@@ -1,36 +1,59 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, ViewContainerRef } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { ListViewEventData } from "nativescript-ui-listview";
-import { Subscription } from "rxjs";
-import { finalize } from "rxjs/operators";
+import {BehaviorSubject, Subscription} from "rxjs";
+import {filter, finalize, tap} from "rxjs/operators";
+import { combineLatest} from "rxjs";
 import { ObservableArray } from "tns-core-modules/data/observable-array";
 
 import { Car } from "./shared/car.model";
 import { CarService } from "./shared/car.service";
+import {ModalDialogService, ModalDialogParams, ModalDialogOptions} from "nativescript-angular/modal-dialog";
+import {ModalViewActionBarComponent} from "~/app/cars/cars.module";
+
 
 @Component({
     selector: "CarsList",
     templateUrl: "./car-list.component.html",
-    styleUrls: ["./car-list.component.scss"]
+    styleUrls: ["./car-list.component.scss"],
+    providers: [ModalDialogService]
 })
 export class CarListComponent implements OnInit, OnDestroy {
     private _isLoading: boolean = false;
     private _cars: ObservableArray<Car> = new ObservableArray<Car>([]);
     private _dataSubscription: Subscription;
+    private _filterSubject: BehaviorSubject<any>  = new BehaviorSubject(null);
+    private filterDef: any;
 
     constructor(
         private _carService: CarService,
-        private _routerExtensions: RouterExtensions
+        private _routerExtensions: RouterExtensions,
+        private _modalService: ModalDialogService,
+        private viewContainerRef: ViewContainerRef
     ) { }
 
     ngOnInit(): void {
         if (!this._dataSubscription) {
             this._isLoading = true;
 
-            this._dataSubscription = this._carService.load()
-                .pipe(finalize(() => this._isLoading = false))
+            this._dataSubscription = combineLatest(
+                this._carService.load().pipe(finalize(() => this._isLoading = false)),
+                this._filterSubject.pipe(filter(i=>i!==undefined)),
+                (cars, b) => {
+                    if (b === null) {
+                        this.createFilterFromData(cars);
+                        return cars;
+                    } else {
+                        this.filterDef = b;
+                        return cars.filter(car => {
+                            return (this.filterDef.types.every(i => i.val) || this.filterDef.types.every(i => !i.val) ||
+                                this.filterDef.types.find(i => i.name === car.type ).val) && (this.filterDef.colors.every(i => i.val) || this.filterDef.colors.every(i => !i.val) ||
+                                this.filterDef.colors.find(i => i.name === car.color ).val);
+                        });
+                    }
+
+                })
                 .subscribe((cars: Array<Car>) => {
-                    console.log(JSON.stringify(cars, null, '\t'));
                     this._cars = new ObservableArray(cars);
                     this._isLoading = false;
                 });
@@ -52,6 +75,25 @@ export class CarListComponent implements OnInit, OnDestroy {
         return this._isLoading;
     }
 
+    private createFilterFromData(cars: Car[]) {
+        this.filterDef = {
+            types: [
+                {name: 'top', val: false},
+                {name: 'bottom', val: false},
+                {name: 'one piece', val: false}
+
+            ],
+            colors: cars.reduce((accum, car) => {
+                const {color: name = null} = <any>car;
+                const val = false;
+                if (name && !accum.find(i => i.name === name)) {
+                    accum.push({name, val});
+                }
+                return accum;
+            }, [])
+        };
+    }
+
     onCarItemTap(args: ListViewEventData): void {
         const tappedCarItem = args.view.bindingContext;
 
@@ -64,5 +106,16 @@ export class CarListComponent implements OnInit, OnDestroy {
                     curve: "ease"
                 }
             });
+    }
+
+    showFiltersDialog() {
+        const opts: ModalDialogOptions = {
+            context: { filter: JSON.parse(JSON.stringify(this.filterDef)) },
+            fullscreen: false,
+            viewContainerRef: this.viewContainerRef
+        };
+        this._modalService.showModal(ModalViewActionBarComponent, opts).then(data => {
+            this._filterSubject.next(data);
+        });
     }
 }
--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -1,5 +1,5 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptCommonModule } from "nativescript-angular/common";
+import {Component, NgModule, NO_ERRORS_SCHEMA, OnInit} from "@angular/core";
+import { NativeScriptCommonModule, ModalDialogParams } from "nativescript-angular/common";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptUIListViewModule } from "nativescript-ui-listview/angular";
 
@@ -10,13 +10,36 @@ import { MyListSelectorComponent } from "./car-detail-edit/my-list-selector/my-l
 import { CarDetailComponent } from "./car-detail/car-detail.component";
 import { CarListComponent } from "./car-list.component";
 import { CarsRoutingModule } from "./cars-routing.module";
+import { TNSCheckBoxModule } from '@nstudio/nativescript-checkbox/angular';
+
+@Component({
+    moduleId: module.id,
+    template: "<FlexboxLayout flexDirection=\"column\" width=\"300\" height=\"auto\" backgroundColor=\"white\">\n    <Label [text]=\"\'Costume Types\'\" class=\"modal-input__header\"></Label>\n    \n    <CheckBox #el *ngFor=\"let type of _params.context.filter.types\" text=\"{{type.name | titlecase }}\" class=\'modal-input__list-item\' checked=\"{{!!type.val}}\" (checkedChange)=\"onTypeFilterCheckedChange(el, _params.context.filter.types)\"></CheckBox>\n    <Label [text]=\"\'Costume Colors\'\" class=\"modal-input__header\"></Label>\n    <CheckBox #el *ngFor=\"let color of _params.context.filter.colors\" fillColor=\"{{color.name}}\" text=\"{{color.name | titlecase }}\" class=\'modal-input__list-item\' checked=\"{{!!color.val}}\" (checkedChange)=\"onTypeFilterCheckedChange(el, _params.context.filter.colors)\"></CheckBox>\n    <Button alignSelf=\'flex-end\' class=\"-outline\" row=\"3\" text=\"Apply filters\" ios:visibility=\"collapsed\" horizontalAlignment=\"right\" (tap)=\"applyFilters()\"></Button>\n</FlexboxLayout>",
+    styleUrls: ["./car-detail-edit/my-list-selector/my-list-selector-modal-view.component.scss"]
+})
+export class ModalViewActionBarComponent implements OnInit {
+    constructor(public _params: ModalDialogParams ) {
+        console.log(_params.context.filter);
+    }
+
+    ngOnInit(): void {}
+    applyFilters(): void {
+        this._params.closeCallback(this._params.context.filter);
+    }
+
+    onTypeFilterCheckedChange(el, arr) {
+        const type = el.text.toLowerCase();
+        arr.find(({name})=>name===type).val = el.checked;
+    }
+}
 
 @NgModule({
     imports: [
         CarsRoutingModule,
         NativeScriptCommonModule,
         NativeScriptFormsModule,
-        NativeScriptUIListViewModule
+        NativeScriptUIListViewModule,
+        TNSCheckBoxModule
     ],
     declarations: [
         CarListComponent,
@@ -24,10 +47,12 @@ import { CarsRoutingModule } from "./cars-routing.module";
         CarDetailEditComponent,
         MyListSelectorComponent,
         MyListSelectorModalViewComponent,
-        MyImageAddRemoveComponent
+        MyImageAddRemoveComponent,
+        ModalViewActionBarComponent
     ],
     entryComponents: [
-        MyListSelectorModalViewComponent
+        MyListSelectorModalViewComponent,
+        ModalViewActionBarComponent
     ],
     providers: [],
     schemas: [
